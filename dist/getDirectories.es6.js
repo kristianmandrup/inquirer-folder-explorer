ac6d2301230b6425a88cd18f67ed2a5f944c8bba
Kristian Mandrup <kmandrup@gmail.com>
2016-11-03 13:24:48 +0100

    add dist

---
+++ dist/getDirectories.es6.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (srcpath) {
+  return fs.readdirSync(srcpath).filter(function (file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+};
---
+++ dist/test/index.es6.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const test = require('tape');
+const fn = reuquire('../index');
+
+// tests coming soon
+test('Assert object values', t => {
+  t.plan(1);
+  t.equal(true, true);
+});
